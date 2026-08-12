3cc978a9a5ea8e6a367c250a35ec14d63e957467
yepMizu <luyashthapa123@gmail.com>
2026-08-12 22:36:21 +0545

    Initialized values of x and y

---
+++ main.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+# The equation is y = 3x + 5
+# Lets try predict this using simplest form of NN
+
+
+#x values:
+x.np = ([
+    0,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9
+])
+
+#y values:
+y.np = ([
+    5,
+    8,
+    11,
+    14,
+    17,
+    20,
+    23,
+    26,
+    29,
+    32
+])
+
+
