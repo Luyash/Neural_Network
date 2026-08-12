46d112999ce66c82de23e609e78d237a3da4752d
yepMizu <luyashthapa123@gmail.com>
2026-08-12 23:43:28 +0545

    okay all logics and loops done

--- main.py
+++ main.py
@@ -40,13 +40,13 @@ b = 0.2   #bias
 learning_rate = 0.01
 
 #The main part:
-for epoch in range(1,250):
+for epoch in range(1,801):
     prediction = w * x + b
 
     #loss:
     loss = np.mean((prediction - y) ** 2)
     print(f"The loss for epoch: {epoch} is {loss}")
-    time.sleep(0.2)
+    time.sleep(0.01)
 
     gradient_weight = np.mean(2 * (prediction - y) *x)
     gradient_bias = np.mean(2* (prediction - y))
@@ -55,7 +55,15 @@ for epoch in range(1,250):
     w = w - learning_rate * gradient_weight
     b = b - learning_rate * gradient_bias
 
+print("="*70)
+print("weight:", w)
+print("bias:", b)
+print("="*70)
+print(f"The predicted function is y = {w:.4f}x + {b:.4f}")
+a = int(input("Enter the value of x --> "))
+print("="*70)
+ans = w*a + b
+print(f"The predicted value of y using y = {int(w)}x + {int(b)} is y = {ans}")
 
-print(f"The predicted function is y = {w}x + {b}")
 
 
