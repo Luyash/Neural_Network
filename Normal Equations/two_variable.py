aea2b7cce5535fd49ca9994ae24d231db5d47584
yepMizu <luyashthapa123@gmail.com>
2026-08-13 20:15:36 +0545

    initialized x1 and x2 values

---
+++ Normal Equations/two_variable.py
@@ -0,0 +1,18 @@
+import numpy as np
+import time
+
+# We will now try to predict the equation y = 3x + 2z + 1
+# This has 2 unknown variables
+
+# x values:
+x1 = np.array([
+    0, 1, 2, 3, 4,
+    5, 6, 7, 8, 9
+])
+
+x2 = np.array([
+    -5, -4, -3, -2,
+    -1, 0, 1, 2, 3
+])
+
+
