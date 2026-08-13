7568a857eec021337f59c5b4c2e473bc538a7e41
yepMizu <luyashthapa123@gmail.com>
2026-08-13 21:28:49 +0545

    Everything done

--- Normal Equations/two_variable.py
+++ Normal Equations/two_variable.py
@@ -36,6 +36,7 @@ for epoch in range(1, 801):
 
     print(f"The loss of epoch number: {epoch} is --> {loss}")
 
+    # Calculate Gradients:
     w1_gradient = np.mean(2 * (predicted - y) * x1)
     w2_gradient = np.mean(2 * (predicted - y) * x2)
     b_gradient = np.mean(2 * (predicted - y))
@@ -44,7 +45,31 @@ for epoch in range(1, 801):
     w1 = w1 - learning_rate * w1_gradient
     w2 = w2 - learning_rate * w2_gradient
     b = b - learning_rate * b_gradient
-    
+
+    print("="*70)
+    print(f"Predicted equation is --> y = {w1}x1 + {w2}x2 + {b}")
+    print("="*70)
+
+print()
+print("Prediction is completed")
+print("="*70)
+print()
+print(f"Final predicted equation is --> y = {round(w1)}x1 + {round(w2)}x2 + {round(b)}")
+print()
+print("="*70)
+print()
+print("="*70)
+a = int(input("Enter the value of x1 --> "))
+c = int(input("Enter the value of x2 --> "))
+print("="*70)
+print()
+result = w1 * a + w2 * c + b
+print(f"The value of y is --> {result} ")
+print("="*70)
+
+
+
+
      
     
 
