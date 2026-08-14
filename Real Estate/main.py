826cbabf15d7b0e379bcd9a5272f95b2b79a5584
yepMizu <luyashthapa123@gmail.com>
2026-08-14 22:31:41 +0545

    Got data of everything

--- Real Estate/main.py
+++ Real Estate/main.py
@@ -1,5 +1,23 @@
 import pandas as pd
+import numpy as np
 
-data = pd.read_excel("Real estate valuation data set.xlsx")
 
-print(data)
+# Take the pandas DataFrame called data and convert it into a NumPy array
+data = pd.read_excel("Real Estate/Real estate valuation data set.xlsx")
+
+
+# this makes array like ndarray datatype of numpy 
+array = data.to_numpy()
+
+# Get all data:
+transaction_date = array[:, 1]
+house_age        = array[:, 2]
+mrt_distance     = array[:, 3]
+stores           = array[:, 4]
+latitude         = array[:, 5]
+longitude        = array[:, 6]
+price            = array[:, 7]
+
+
+
+
