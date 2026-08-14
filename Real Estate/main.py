49d638fea21c2fc27e9127d55010872bb3ed8b17
yepMizu <luyashthapa123@gmail.com>
2026-08-14 23:09:31 +0545

    Start of the neural network

--- Real Estate/main.py
+++ Real Estate/main.py
@@ -9,16 +9,6 @@ data = pd.read_excel("Real Estate/Real estate valuation data set.xlsx")
 # this makes array like ndarray datatype of numpy 
 array = data.to_numpy()
 
-# Get all data:
-transaction_date = array[:, 1]
-house_age        = array[:, 2]
-mrt_distance     = array[:, 3]
-stores           = array[:, 4]
-latitude         = array[:, 5]
-longitude        = array[:, 6]
-price            = array[:, 7]
-
-
 # Inputs and Targets
 X = array[:, 1:7]
 y = array[:, 7]
@@ -39,6 +29,9 @@ train_sd = X_train.std(axis=0)
 # Standardize
 X_train_scaled = (X_train - train_mean) / train_sd
 X_test_scaled = (X_test - train_mean) / train_sd
+
+# Okay now the training stuff below:
+
 
 
 
