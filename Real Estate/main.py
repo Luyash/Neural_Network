391dd47a48a4c601c266a985f06263f44b19b2c3
yepMizu <luyashthapa123@gmail.com>
2026-08-14 22:59:23 +0545

    Standardization done and splitting also

--- Real Estate/main.py
+++ Real Estate/main.py
@@ -19,9 +19,18 @@ longitude        = array[:, 6]
 price            = array[:, 7]
 
 
-# Splitting data to 80% train and 20% test:
-X_train = X[:80%]
-X_test = X[80%:]
+# Inputs and Targets
+X = array[:, 1:7]
+y = array[:, 7]
+
+# Splitting the data into 80% training and 20% test
+split_index = int(len(X) * 0.8)  # Gives 80% vaneko kati ho vanera 
+
+X_train = X[:split_index] # First 80%
+X_test = X[split_index:]  # Remaining 20%
+
+y_train = y[:split_index]  # First 80%
+y_test = y[split_index:]   # Remaining 20%
 
 # Calculate mean, SD for Standardization:
 train_mean = X_train.mean(axis=0)
@@ -30,5 +39,6 @@ train_sd = X_train.std(axis=0)
 # Standardize
 X_train_scaled = (X_train - train_mean) / train_sd
 X_test_scaled = (X_test - train_mean) / train_sd
+
 
 
