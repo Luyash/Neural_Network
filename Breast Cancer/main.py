696cc96119114e4e081f7da5b8691ff5abdb8e3e
yepMizu <luyashthapa123@gmail.com>
2026-08-18 23:30:33 +0545

    Did randomizer for 80% 20% split also calculated mean and standard deviation for Standardization of the data

--- Breast Cancer/main.py
+++ Breast Cancer/main.py
@@ -16,4 +16,25 @@ X = data[:, 2:32]   # columns 2 through 31 → 30 inputs
 
 y = data[:, 1:2]    # column 1 → diagnosis
 
+# Adding randomizer for the input data like biccha biccha bata 80% and 20%
+indices = np.random.permutation(len(X))
 
+X = X[indices]
+y = y[indices]
+
+# Splitting the data into 80% training and 20% test
+split_index = int(len(X) * 0.8)  # Gives 80% vaneko kati ho vanera 
+
+X_train = X[:split_index] # First 80%
+X_test = X[split_index:]  # Remaining 20%
+
+y_train = y[:split_index]  # First 80%
+y_test = y[split_index:]   # Remaining 20%
+
+# Calculate mean, SD for Standardization:
+train_mean = X_train.mean(axis=0)
+train_sd = X_train.std(axis=0)
+
+# Standardize
+X_train_scaled = (X_train - train_mean) / train_sd
+X_test_scaled = (X_test - train_mean) / train_sd
