e4cb0be2699d245155edbdcb05213883273cc989
yepMizu <luyashthapa123@gmail.com>
2026-08-19 22:19:49 +0545

    All functions logic written

--- Breast Cancer/functions.py
+++ Breast Cancer/functions.py
@@ -1,14 +1,13 @@
 import numpy as np
 import math
 
-
-def relu(x):
+def relu(x):   # Relu ---> negative and 0 return 0 natra return input number
     if x <= 0:
         return 0
     else:
         return x
 
-def sigmoid(x):
+def sigmoid(x):    # Sigmoid function --> y = 1/ ( 1+ e^(-x) )
     y = 1 + math.exp(-x)
     y = 1 / y
     return y
