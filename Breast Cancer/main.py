d95ad70a089c459051a19b363884f96ba4efd17f
yepMizu <luyashthapa123@gmail.com>
2026-08-19 22:47:29 +0545

    Start of Backprop

--- Breast Cancer/main.py
+++ Breast Cancer/main.py
@@ -76,7 +76,14 @@ for epoch in range (1,20000,1):
 
     # Loss for the predicted values:
     loss = BCE(y_train , predicted)
+
+    # =====================
+    # BACKPROPAGATION
+    # =====================
+
     
+
+
 
 
 
